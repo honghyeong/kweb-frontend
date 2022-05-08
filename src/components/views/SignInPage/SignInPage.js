04c1e3e7e890b5c5cc5c6cf69a35018b69ca022a
honghyeong <scarletgold0601@gmail.com>
2022-05-08 22:26:29 +0900

    feat: signin structure

--- src/components/views/SignInPage/SignInPage.js
+++ src/components/views/SignInPage/SignInPage.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SignInPage() {
-  return <div>SignInPage</div>;
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onIdHandler = (event) => {
+    setId(event.currentTarget.value);
+  };
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <>
+      <div className="up-box">
+        <div>창의적 미래 인재를 양성하고 어쩌고~</div>
+        <div className="login-box">
+          <div>수강신청 시스템</div>
+          <div>COURSE REGISTRATION</div>
+          <form onSubmit={onSubmitHandler}>
+            <input type="checkbox" />
+            <input
+              type="text"
+              placeholder="ID"
+              value={id}
+              onChange={onIdHandler}
+            />
+            <input
+              type="password"
+              placeholder="PASSWORD"
+              value={password}
+              onChange={onPasswordHandler}
+            />
+            <button>LOGIN</button>
+          </form>
+        </div>
+        <div></div>
+      </div>
+      <div className="down-box"></div>
+    </>
+  );
 }
 
 export default SignInPage;
