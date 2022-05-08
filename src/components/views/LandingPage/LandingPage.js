98491689f7fbc2ae9dcb22cc651dd0b10efa677a
honghyeong <scarletgold0601@gmail.com>
2022-05-08 19:13:00 +0900

    feat: axios localhost cors test

--- src/components/views/LandingPage/LandingPage.js
+++ src/components/views/LandingPage/LandingPage.js
@@ -2,6 +2,11 @@ import axios from "axios";
 import React, { useEffect } from "react";
 
 function LandingPage() {
+  useEffect(() => {
+    axios
+      .get("http://localhost:3002/api/connect")
+      .then((response) => console.log(response));
+  }, []);
   return <div>LandingPage</div>;
 }
 
