dfb5ecd8e044a195925a23dc7d0172f2cc5b0fdc
honghyeong <scarletgold0601@gmail.com>
2022-05-08 19:12:05 +0900

    feat: init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import SignInPage from "./components/views/SignInPage/SignInPage";
+import SignUpPage from "./components/views/SignUpPage/SignUpPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/signup" component={SignUpPage} />
+          <Route path="/signin" component={SignInPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+
+function LandingPage() {
+  return <div>LandingPage</div>;
+}
+
+export default LandingPage;
