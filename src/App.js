d8ff46cd3b512c5b5e487297fd1e86f9fc3381df
honghyeong <scarletgold0601@gmail.com>
2022-06-12 01:31:14 +0900

    feat: logout button on navigation bar

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LandingPage from "./components/views/LandingPage/LandingPage";
 import SignInPage from "./components/views/SignInPage/SignInPage";
 import SignUpPage from "./components/views/SignUpPage/SignUpPage";
+import NavigationBar from "./components/views/NavigationBar/NavigationBar";
 
 function App() {
   return (
     <Router>
-      <div>~
+      <div>
+        ~
         <Switch>
           <Route exact path="/" component={SignInPage} />
           <Route path="/signup" component={SignUpPage} />
           <Route path="/landing" component={LandingPage} />
+          <Route path="/logout" component={NavigationBar} />
         </Switch>
       </div>
     </Router>
--- src/components/views/NavigationBar/NavigationBar.js
+++ src/components/views/NavigationBar/NavigationBar.js
@@ -1,7 +1,23 @@
+import axios from "axios";
 import React from "react";
 
-function NavigationBar() {
-  return <div>NavigationBar</div>;
+function NavigationBar(props) {
+  const logOutHandler = (event) => {
+    axios.post("/api/auth/logout").then((response) => {
+      if (response.data.logoutSuccess) {
+        props.history.push("/");
+      } else {
+        alert("LogOut Failed");
+      }
+    });
+  };
+
+  return (
+    <>
+      <div>NavigationBar</div>
+      <button onSubmit={logOutHandler}>로그아웃</button>
+    </>
+  );
 }
 
 export default NavigationBar;
