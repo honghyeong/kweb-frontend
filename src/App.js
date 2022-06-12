aa7d2630295ef0409d076029c11c5938d109d590
honghyeong <scarletgold0601@gmail.com>
2022-06-13 00:47:08 +0900

    refactor: instructor, student lectures in lecture dir

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import SignInPage from "./components/views/SignInPage/SignInPage";
 import SignUpPage from "./components/views/SignUpPage/SignUpPage";
 import NavigationBar from "./components/views/NavigationBar/NavigationBar";
 import Lectures from "./components/views/Lectures/Lectures";
-import InstructorLectures from "./components/views/InstructorLectures/InstructorLectures";
-import StudentLectures from "./components/views/StudentLectures/StudentLectures";
+import InstructorLectures from "./components/views/Lectures/InstructorLectures";
+import StudentLectures from "./components/views/Lectures/StudentLectures";
 
 function App() {
   return (
     <Router>
       <div>
-        ~
         <Switch>
           <Route exact path="/" component={SignInPage} />
           <Route path="/signup" component={SignUpPage} />
